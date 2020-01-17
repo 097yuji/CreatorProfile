6d70dba9b3932b7da098ffbf77424a8626653da1
奥名 優次 <okuna@ad.snapshot.co.jp>
2020-01-17 18:26:48 +0900

    facebookの埋め込みタイムラインをテストユーザーのものに変更

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -8,39 +8,43 @@ class ProfileController extends Controller
 {
     public function profile()
     {
-        // facebook ページアクセストークン
-        $fb_token = 'EAAehU7Pb8ZAUBABCZAM1DBo3wrNswZBncFnKwACSSZBnXgbo6QDzhs5aZBI08pm7nPQAuNNN30JBwVr9mfLmNuyAt0Gz6iAW7Qziw7LJs0ItiraaVDF1YQEwzU50WnAdyX3RBCHIDPc64KoHwA3N826nVWJlh2W9Ds8Mtjx4YbNQdoYCMdduGlyhlwtlJnKcZD';
+        // facebook ページアクセストークン (API申請用テストアカウントのページ用)
+        $fb_token = 'EAAehU7Pb8ZAUBAAN5kNAOj3ZCKAdwz257HuTDui3ulptJVKHfoLqZCJxG5eI4ZAhUVLAZASA1ova4a89YRmDk2EC3MGbOhBMY285Koiv7KG18kVvQXcpcC5eFFGer3inO3ZCU10gA2Jyais3AFIN91xaaqWunX7q0EVYg5rbCogalZBdPGgBZB5xnmW1eZAasDXh6wkhPE65JSZCMUMAn5GLmY';
 
-        // facebook ユーザーのページID
-        $user_page_id = '112001893565201';
+        // facebook ユーザーのページID (API申請用テストアカウントのページ用)
+        $user_page_id = '108554017352373';
 
         // facebookのタイムラインから各投稿のIDとキャプションを取得
         $post_id = json_decode(@file_get_contents('https://graph.facebook.com/' . $user_page_id . '/feed?fields=message&access_token=' . $fb_token), true);
 
         // 特定のハッシュタグで絞った投稿の[投稿ID, キャプション]を 配列$hash_post[]に格納
         foreach ($post_id['data'] as $data) {
-            if (strpos($data['message'], '#てすと') !== false) {
+            if (isset($data['message']) && strpos($data['message'], '#てすと') !== false) {
                 $hash_post[] = [$data['id'], $data['message']];
             }
         }
+
         // 各投稿IDから投稿内容データを取得
         foreach ($hash_post as $post) {
 
             $post_data[] = [json_decode(@file_get_contents('https://graph.facebook.com/' . $post[0] . '/attachments?access_token=' . $fb_token), true), $post[1]];
         }
+        // dd($post_data);
 
 
         // 投稿データから画像とキャプションを抜き出し、配列$fb_data[]に格納
         foreach ($post_data as $data) {
-            // 投稿の添付画像が1枚の場合
-            if ($data[0]['data'][0]['type'] == 'photo') {
-                $fb_data[] = ['img' => $data[0]['data'][0]['media']['image']['src'], 'msg' => $data[1]];
-            } else { // 投稿の添付画像が複数の場合
-                foreach ($data[0]['data'][0]['subattachments']['data'] as $medias) {
-                    if ($medias !== end($data[0]['data'][0]['subattachments']['data'])) { // 最終画像下以外キャプション表示しない
-                        $fb_data[] = ['img' => $medias['media']['image']['src'], null];
-                    } else {
-                        $fb_data[] = ['img' => $medias['media']['image']['src'], 'msg' => $data[1]];
+            if (isset($data[0]['data'][0])) { // 添付データがある場合
+
+                if ($data[0]['data'][0]['type'] == 'photo') { // 投稿の添付画像が1枚の場合
+                    $fb_data[] = ['img' => $data[0]['data'][0]['media']['image']['src'], 'msg' => $data[1]];
+                } elseif ($data[0]['data'][0]['type'] == 'album') { // 投稿の添付画像が複数の場合
+                    foreach ($data[0]['data'][0]['subattachments']['data'] as $medias) {
+                        if ($medias !== end($data[0]['data'][0]['subattachments']['data'])) { // 最終画像下以外キャプション表示しない
+                            $fb_data[] = ['img' => $medias['media']['image']['src'], null];
+                        } else {
+                            $fb_data[] = ['img' => $medias['media']['image']['src'], 'msg' => $data[1]];
+                        }
                     }
                 }
             }
